2bdb2da00815aa34c6ed977107bdd4e346adf44a
Miguel Angel Inga Ninahuanca <miguel.inga@tecsup.edu.pe>
2021-12-03 11:08:14 -0500

    EC2 db

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'db-cars.cneqgdew4rw7.us-east-1.rds.amazonaws.com',
-        user: 'admin',
-        password: 'miguelinga',
+        host: '54.172.40.0',
+        user: 'miguel',
+        password: '12345678main',
         database: 'db_cars'
     }
 
